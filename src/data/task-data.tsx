3e556214ea91539f05fc5216fe94e3cf5078f2bc
Constanza Aceves Rodriguez <cacevesr@u.rochester.edu>
2022-05-04 13:05:11 -0400

    Add example data for demo

---
+++ src/data/task-data.tsx
@@ -0,0 +1,35 @@
+export const demoTasks = [
+    {
+      id: 1,
+      language: 'Kihavu',
+      title: 'Lexicon',
+      description: 'Kihavu dictionary',
+      nusers: 4,
+      max_users: 15,
+    },
+    {
+      id: 2,
+      language: 'Kihavu',
+      title: 'Topological relations',
+      description: 'Spatial prepositions in Kihavu',
+      nusers: 0,
+      max_users: 15,
+    },
+    {
+      id: 3,
+      language: 'Kihavu',
+      title: 'Possessives',
+      description: 'Possessive paradigm in Kihavu',
+      nusers: 1,
+      max_users: 15,
+    },
+    {
+      id: 4,
+      language: 'Spanish',
+      title: 'Lexicon',
+      description: 'Mexican Spanish Dictionary',
+      nusers: 0,
+      max_users: 15,
+    },
+  ];
+  
