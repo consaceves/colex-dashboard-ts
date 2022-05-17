d4afcb2b42f87c53b7f232d3c422a927d0ef9316
Constanza Aceves Rodriguez <cacevesr@u.rochester.edu>
2022-05-17 16:13:01 -0400

    Add boilerplate stuff and demo table

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from 'react-bootstrap/Table';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import BootstrapTable, { BootstrapTableProps } from 'react-bootstrap-table-next'
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
+
+import { demoTasks } from './data/task-data.js';
+import { hover } from '@testing-library/user-event/dist/hover';
+
+
+const columns = [
+  {
+    dataField: "language",
+    text: "Language",
+    sort: true
+  },
+  {
+    dataField: "title",
+    text: "Title",
+    sort: true
+  },
+  {
+    dataField: "description",
+    text: "Description",
+  },
+  {
+    dataField: "nusers",
+    text: "Contributors"
+  },
+  {
+    dataField: "max_users",
+    text: "Maximum # contributors"
+  }
+];
+
+const MyExportCSV = (props: { onExport: () => void; }) => {
+  const handleClick = () => {
+    props.onExport();
+  };
+  return (
+    <div>
+      <button className="btn btn-success" onClick={ handleClick }>Export as CSV</button>
+    </div>
+  );
+};
+
+const { SearchBar } = Search;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container>
+        <ToolkitProvider
+        bootstrap4
+        keyField="language"
+        data={demoTasks}
+        columns={columns}
+        exportCSV
+        search
         >
-          Learn React
-        </a>
-      </header>
+          {
+            (props) => (
+              <Container>
+                <Row>
+                  <Col><SearchBar { ...props.searchProps }/></Col>
+                </Row>
+                <hr />
+                <BootstrapTable hover { ...props.baseProps } />
+                <Col><MyExportCSV { ...props.csvProps }></MyExportCSV></Col>
+              </Container>
+            )
+          }
+        </ToolkitProvider>
+      </Container>
     </div>
   );
 }
